1a994a6ca5bd3f2ced954bcf45838d0d0f9726aa
wowbios <wowbios@yandex.ru>
2022-05-24 23:31:03 +0300

    Class interaction

---
+++ src/FluentMermaid/ClassDiagram/Extensions/RelationLinkExtensions.cs
@@ -0,0 +1,14 @@
+﻿using FluentMermaid.ClassDiagram.Enums;
+
+namespace FluentMermaid.ClassDiagram.Extensions;
+
+internal static class RelationLinkExtensions
+{
+    public static string Render(this RelationLink relationLink)
+        => relationLink switch
+        {
+            RelationLink.Solid => "--",
+            RelationLink.Dashed => "..",
+            _ => throw new ArgumentOutOfRangeException(nameof(relationLink), relationLink, null)
+        };
+}
