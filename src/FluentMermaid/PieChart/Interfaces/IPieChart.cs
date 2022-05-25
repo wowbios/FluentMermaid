ff1baaef78a6ff515353c5de97ed5f4d09399a2e
Aleksey <wowbios@yandex.ru>
2022-05-25 22:54:37 +0300

    Pie chart (#10)

---
+++ src/FluentMermaid/PieChart/Interfaces/IPieChart.cs
@@ -0,0 +1,18 @@
+﻿namespace FluentMermaid.PieChart.Interfaces;
+
+public interface IPieChart
+{
+    string? Title { get; }
+
+    bool ShowData { get; }
+
+    IValue Add(string name, double value);
+
+    IValue Add(string name, float value);
+
+    IValue Add(string name, int value);
+
+    IValue Add(string name, decimal value);
+
+    string Render();
+}
---
+++ src/FluentMermaid/PieChart/Interfaces/IValue.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+namespace FluentMermaid.PieChart.Interfaces;
+
+public interface IValue : IRenderTo<StringBuilder>
+{
+    string Title { get; }
+    
+    double? DoubleValue { get; }
+    
+    float? FloatValue { get; }
+    
+    int? IntValue { get; }
+    
+    decimal? DecimalValue { get; }
+}
